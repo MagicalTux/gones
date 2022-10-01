a344f7cf45ff60414d638c11795a632dacf29e76
Mark Karpeles <magicaltux@gmail.com>
2022-10-02 02:23:17 +0900

    move memory access methods, add mask flags

--- ppu/flags.go
+++ ppu/flags.go
@@ -1,10 +1,12 @@
 package ppu
 
 const (
+	// STATUS
 	SpriteOverflow byte = 0x20
 	SpriteZeroHit  byte = 0x40 // Set when a nonzero pixel of sprite 0 overlaps a nonzero background pixel
 	VBlankStarted  byte = 0x80
 
+	// CTRL
 	NameTableMask   byte = 0x03 // 0, 1, 2 or 3
 	LargeIncrements byte = 0x04 // VRAM address increment per CPU read/write of PPUDATA (0: add 1, going across; 1: add 32, going down)
 	AltSprites      byte = 0x08 // Sprite pattern table address for 8x8 sprites (0: $0000; 1: $1000; ignored in 8x16 mode)
@@ -12,6 +14,16 @@ const (
 	WideSprites     byte = 0x20 // Sprite size (0: 8x8 pixels; 1: 8x16 pixels – see PPU OAM#Byte 1)
 	PpuMaster       byte = 0x40 // PPU master/slave select (0: read backdrop from EXT pins; 1: output color on EXT pins)
 	GenerateNMI     byte = 0x80 // Generate an NMI at the start of the vertical blanking interval (0: off; 1: on)
+
+	// MASK
+	Greyscale       byte = 0x01 // Greyscale (0: normal color, 1: produce a greyscale display)
+	ShowLeftBg      byte = 0x02 // 1: Show background in leftmost 8 pixels of screen, 0: Hide
+	ShowLeftSprites byte = 0x04 // 1: Show sprites in leftmost 8 pixels of screen, 0: Hide
+	ShowBg          byte = 0x08 // 1: Show background
+	ShowSprites     byte = 0x10 // 1: Show sprites
+	EmphasizeRed    byte = 0x20 // Emphasize red (green on PAL/Dendy)
+	EmphasizeGreen  byte = 0x40 // Emphasize green (red on PAL/Dendy)
+	EmphasizeBlue   byte = 0x80 // Emphasize blue
 
 	PPUCTRL   = 0
 	PPUMASK   = 1
---
+++ ppu/memory.go
@@ -0,0 +1,122 @@
+package ppu
+
+import (
+	"log"
+	"unsafe"
+)
+
+func (p *PPU) MemRead(offset uint16) byte {
+	// only care about first 3 bits (&0x7)
+
+	switch offset & 7 {
+	case PPUSTATUS: // PPU status
+		stat := p.stat
+		p.stat &= ^VBlankStarted // always clear VBlankStarted when reading PPU STATUS
+		p.W = false              // reading PPUSTATUS resets the PPUADDR latch
+		if p.scanline == 241 && p.cycle == 0 {
+			// special case, hide vblank flag and don't send the NMI
+			p.vblankNMI = false
+			stat &= ^VBlankStarted
+		}
+		if p.scanline == 241 && p.cycle <= 2 {
+			// if we are within 2 PPU clocks of setting p.vblankFlag we should inhibit the NMI
+			p.vblankNMI = false
+		}
+		return stat
+	case OAMDATA:
+		// read OAM data
+		if p.oamAddr&0x03 == 0x02 {
+			// see: https://www.nesdev.org/wiki/PPU_OAM#Byte_2
+			// bits 2, 3, 4 of byte 2 always return zero
+			return p.OAM[p.oamAddr] & 0xe3
+		}
+		return p.OAM[p.oamAddr]
+	case PPUDATA:
+		// read from memory at address p.ppuAddr
+		// See: https://www.nesdev.org/wiki/PPU_registers#The_PPUDATA_read_buffer_(post-fetch)
+		res := p.readBuf
+		p.readBuf = p.Memory.MemRead(p.V & 0x3fff)
+		if p.V > 0x3f00 {
+			// return palette data instead
+			res = p.Palette[p.V%0x20]
+		}
+		// increment p.V
+		if !p.getFlag(LargeIncrements) {
+			p.V += 1
+		} else {
+			p.V += 32
+		}
+		return res
+	default:
+		log.Printf("Unhandled PPU read: $%04x", offset)
+	}
+	return 0
+}
+
+func (p *PPU) MemWrite(offset uint16, val byte) byte {
+	// only care about first 3 bits (&0x7)
+	switch offset & 7 {
+	case PPUCTRL:
+		if !p.getFlag(VBlankStarted) && val&VBlankStarted == VBlankStarted {
+			// enabling VBlankStarted will reset p.vblankNMI to p.vblankFlag and may trigger an extra NMI
+			p.vblankNMI = p.vblankFlag
+		}
+		p.ctrl = val
+	case PPUMASK:
+		p.mask = val
+	case OAMADDR:
+		p.oamAddr = val
+		return 0
+	case OAMDATA:
+		// write value & increment addr
+		p.OAM[p.oamAddr] = val
+		p.oamAddr += 1
+		return 0
+	case PPUSCROLL:
+		// PPUSCROLL and PPUADDR share registers, see https://www.nesdev.org/wiki/PPU_scrolling#Register_controls
+		if !p.W {
+			p.T = (p.T & 0xffe0) | (uint16(val) >> 3)
+			p.X = val & 0x07
+			p.W = true
+		} else {
+			p.T = (p.T & 0x8fff) | ((uint16(val) & 0x07) << 12)
+			p.T = (p.T & 0xfc1f) | ((uint16(val) & 0xf8) << 2)
+			p.W = false
+		}
+		return 0
+	case PPUADDR:
+		if !p.W {
+			p.T = (p.T & 0x80ff) | ((uint16(val) & 0x3f) << 8)
+			p.W = true
+		} else {
+			p.T = (p.T & 0xFF00) | uint16(val)
+			p.V = p.T
+			p.W = false
+		}
+		return 0
+	case PPUDATA:
+		if p.V > 0x3f00 {
+			// write to palette
+			p.Palette[p.V%0x20] = val
+		} else {
+			p.Memory.MemWrite(p.V&0x3fff, val)
+		}
+		// increment p.V
+		if !p.getFlag(LargeIncrements) {
+			p.V += 1
+		} else {
+			p.V += 32
+		}
+	default:
+		log.Printf("Unhandled PPU write: $%04x = $%02x", offset, val)
+	}
+	return 0
+}
+
+func (p *PPU) Ptr() uintptr {
+	return uintptr(unsafe.Pointer(p))
+}
+
+func (p *PPU) String() string {
+	return "PPU"
+}
